b62c0a4e44eeedc3d9e737556f0e38943e783e0c
Fatmeh <fatmeh.zoabi@e.braude.ac.il>
2025-09-11 04:13:36 +0300

    update about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -21,22 +21,12 @@ export default function AboutPage() {
         return () => observer.disconnect();
     }, []);
 
-    const imageUrl = currentTheme.includes('dark') ? '/lake-dusk1.png' : '/lake-daylight1.png';
-
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className={`backdrop-blur-sm shadow-2xl rounded-3xl p-8 max-w-2xl mx-auto text-center transition-colors duration-500 ${currentTheme.includes('dark') ? 'bg-gray-800/60 text-white' : 'bg-white/30 text-black'}`}>
-                <Image
-                    src={imageUrl}
-                    alt="Lake view"
-                    width={500}
-                    height={300}
-                    className="rounded-lg mb-6 mx-auto"
-                />
-
-                <h1 className={`text-4xl font-extrabold mb-4 transition-colors duration-500 ${currentTheme.includes('dark') ? 'text-emerald-300' : 'text-emerald-700'}`}>About Our Project</h1>
+                <h1 className={`text-4xl font-extrabold mb-4 transition-colors duration-500 ${currentTheme.includes('dark') ? 'text-white-300' : 'text-white-700'}`}>About Our Project</h1>
                 <p className={`text-lg font-semibold transition-colors duration-500 ${currentTheme.includes('dark') ? 'text-gray-300' : 'text-gray-800'}`}>
-                    This project is dedicated to monitoring and protecting our local lake.
+                    This project is dedicated to monitoring and protecting our Sea of Galilee.
                     By allowing users to report pollution and share information, we aim to
                     foster a cleaner, healthier environment for everyone.
                 </p>

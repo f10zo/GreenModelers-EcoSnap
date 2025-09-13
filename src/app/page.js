b775c746045d90a722e4281294f2d2034b7a8222
Fatmeh <fatmeh.zoabi@e.braude.ac.il>
2025-09-13 16:47:19 +0300

    Updating InfoSection

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
     return (
         <main className="relative min-h-screen flex items-center justify-center p-8">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-screen-xl">
+                <InfoSection />
                 <UploadForm />
                 <Gallery />
-                <InfoSection />
             </div>
         </main>
     );
